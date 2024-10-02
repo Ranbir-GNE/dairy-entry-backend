87d82d0722c06bd86d16510c66880fd8696d135c
Ranbir-GNE <ranbir2115113@gndec.ac.in>
2024-10-02 19:23:58 +0530

    ..

--- index.cjs
+++ index.cjs
@@ -7,10 +7,12 @@ const dairyRouter = require("./routes/dairyRoutes.cjs");
 const authMiddleware = require("./middleware/authMiddleware.cjs");
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+
 dbConnect();
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
